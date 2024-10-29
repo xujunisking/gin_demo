c2dc6c82fe104bcc23491c20248e39da2ce32857
dracula <271087757@qq.com>
2024-10-29 21:17:23 +0800

    xujun

---
+++ test02/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	b, _ := ioutil.ReadFile("./hello.txt")
+	_, _ = fmt.Fprintln(w, string(b))
+}
+
+func main() {
+
+	http.HandleFunc("/hello", sayHello)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		fmt.Printf("http server failed, err:%v\n", err)
+		return
+	}
+}
